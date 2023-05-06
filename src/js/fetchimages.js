2a0f52a5a834414858b56ff01b57dbd377a10ae4
Molksy <mikefromenclave@gmail.com>
2023-05-06 21:18:31 +0300

    Update fetchimages.js

--- src/js/fetchimages.js
+++ src/js/fetchimages.js
@@ -1,15 +1,15 @@
-import axios from "axios";
+import axios from 'axios';
 
-export default getFoto
-
+const BASE_URL = 'https://pixabay.com/api/';
 const API_KEY = '36108889-e9e01e89ef65dcf60af3a8e18';
 
-function getFoto(date, page) {
-    const URL = `https://pixabay.com/api/?key=${API_KEY}&q=${date}&image_type=photo&orientation=horizontal&safesearch=true&page=${page}&per_page=39`;
-
-    return axios(URL).then(({ data }) => {
-        if (data.hits.length > 0) {
-            return data
-        }
-    }).catch(console.log)
+export async function fetchImages(value, page = 1, perPage = 40) {
+  try {
+    const response = await axios.get(
+      `${BASE_URL}?key=${API_KEY}&q=${value}&image_type=photo&orientation=horizontal&safesearch=true&page=${page}&per_page=${perPage}`
+    );
+    return response;
+  } catch (error) {
+    console.log(error);
+  }
 }
